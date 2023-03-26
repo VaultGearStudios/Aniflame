267fc4f87ee25d39949dde35ee8d35860041554b
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-03-26 17:06:02 +0000

    Major Frontend Changes quite a bit backednd too

--- assets/js/Search.js
+++ assets/js/Search.js
@@ -1,15 +1,7 @@
-import axios from "axios";
 
-let sEngA = [
-	'9Anime'
-	'AnimeFox'
-	'Animepahe'
-	'BiliBili'
-	'Crunchyroll'
-	'Enime'
-	'GogoAnime'
-	'Zoro'
-];
+var searchV = document.getElementById("search").innerHTML;
+
+let sEngA = ['9Anime','AnimeFox','Animepahe','BiliBili','Crunchyroll','Enime','GogoAnime','Zoro'];
 
 let storedE;
 let pageN = 1;
@@ -18,42 +10,42 @@ function engineStorage(x){
 	storedE = sEngA[x]
 }
 
-function searchWE(searchV){
+function searchWE(){
 	switch(storedE) {
   		case '9Anime':
-			const url = "https://api.consumet.org/anime/9anime/info/"+searchV;
-			const data = async () => {
+			const url1 = "https://api.consumet.org/anime/9anime/info/"+searchV;
+			const data1 = async () => {
    				 try {
-      			 	const { data } = await axios.get(url, {params: {id: searchV}});
-      				return data;
+      			 	const { data1 } = await axios.get(url1, {params: {id: searchV}});
+      				return data1;
    				 } catch (err) {
         			throw new Error(err.message);
     			}
 			};
  
-			console.log(data);
+			console.log(data1);
     		break;
 			
 		case 'AnimeFox':
-   			const url = "https://api.consumet.org/anime/animefox/info?id="searchV;
-			const data = async () => {
+   			const url2 = "https://api.consumet.org/anime/animefox/info?id=" + searchV;
+			const data2 = async () => {
    				 try {
-        			const { data } = await axios.get(url, { params: { id: searchV } });
-        			return data;
+        			const { data2 } = await axios.get(url2, { params: { id: searchV } });
+        			return data2;
     			} catch (err) {
         			throw new Error(err.message);
     			}
 			};
  
-			console.log(data);
+			console.log(data2);
     		break;
 			
 		case 'Animepahe':/*
-   			const url = "https://api.consumet.org/anime/animepahe/info/99318885-5a76-cfa6-3b99-57007bbb7673";
+   			const url3 = "https://api.consumet.org/anime/animepahe/info/99318885-5a76-cfa6-3b99-57007bbb7673";
 				const data = async () => {
   			  		try {
-        				const { data } = await axios.get(url);
-        				return data;
+        				const { data } = await axios.get(url3);
+        				return data3;
     				} catch (err) {
         				throw new Error(err.message);
     				}
@@ -63,43 +55,43 @@ function searchWE(searchV){
     		break;
 			
 		case 'BiliBili':
-   			// Currently No Doccumentation
+   			// Currently No Doccumentation 4
     		break;
 			
 		case 'Crunchyroll':
-   			// Extra Param
+   			// Extra Param 5
     		break;
 			
 		case 'Enime':
-   			// Different type of val
+   			// Different type of val 6
     		break;
 			
 		case 'GogoAnime':
-   				const url = "https://api.consumet.org/anime/gogoanime/info/"searchV;
-				const data = async () => {
+   				const url7 = "https://api.consumet.org/anime/gogoanime/info/" + searchV;
+				const data7 = async () => {
     				try {
-       					 const { data } = await axios.get(url);
-        				 return data;
+       					 const { data7 } = await axios.get(url7);
+        				 return data7;
     				} catch (err) {
         				throw new Error(err.message);
     				}
 				};
  
-			console.log(data);
+			console.log(data7);
     		break;
 			
 		case 'Zoro':
-   			const url = "https://api.consumet.org/anime/zoro/info?id=spy-x-family-17977";
-			const data = async () => {
+   			const url8 = "https://api.consumet.org/anime/zoro/info?id=" + searchV;
+			const data8 = async () => {
     			try {
-       				 const { data } = await axios.get(url, {params: {id: "spy-x-family-17977"}});
-        			 return data;
+       				 const { data8 } = await axios.get(url8, {params: {id: searchV}});
+        			 return data8;
     			} catch (err) {
         			throw new Error(err.message);
     			}
 			};
  
-			console.log(data);
+			console.log(data8);
     		break;
 			
 		default:
--- dep.replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.zip
-    pkgs.nodejs
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server  
-    pkgs.nodejs
-  ];
-}
